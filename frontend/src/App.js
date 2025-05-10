04910f0cd7e6acd0ffb553a2ebc792a63f48e455
tif-car <tif304ca@gmail.com>
2025-05-09 20:39:42 -0500

    debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [msg, setMsg] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5001/api/hello") // 👈 Flask endpoint
+      .then((res) => res.json())
+      .then((data) => setMsg(data.message))
+      .catch((err) => setMsg("Failed to connect to backend"));
+  }, []);
+
   return (
     <div>
-      <h1>To do list</h1>
-      <p>This is the main page.</p>
+      <h1>{msg}</h1>
     </div>
   );
 }
 
 export default App;
+
+
